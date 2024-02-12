d82048acf126dae7c3b47bdf42602e772609df6e
raymonddebra830@gmail.com <raymonddebra830@gmail.com>
2024-02-12 14:25:26 -0500

    Intitial Commit

---
+++ Turtle Racing.py
@@ -0,0 +1,19 @@
+#Raymond
+#Date
+#Description: Turtle Racing Game
+#Revision History
+#Name       Date      Desc
+#scl        2/12/24   Initial code base
+#
+#
+
+
+import time
+import turtle
+import random as rd
+
+start_x = -200
+start_y = 150
+
+wn = turtle.Screen()
+wn.bgcolor("blue")
